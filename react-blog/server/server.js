e732546f7503716e7d29e59dd0ff56733ac29bda
c-dongwon <clzlqndqnd1@gmail.com>
2022-03-03 22:59:01 +0900

    220303

--- react-blog/server/server.js
+++ react-blog/server/server.js
@@ -3,8 +3,8 @@ const app = express();//app 변수를통해 서버를 관리할수있음
 
 let PORT = process.env.PORT || 9612;//9612포트 할당
 
-app.get('/', (req, res) => {
-    res.send(`Response Complate`);
+app.get('/api/host', (req, res) => {
+  res.send({ host : 'React' });
 })
 //여기서 첫번째 인자로 들어간 '/'은 경로를 나타내는 인자를 가지게 됨
 //해당 경로로 이동했을 때에 응답을 렌더하게 된다.
--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      host : '',
+    }
+  }
+
+  componentDidMount() {
+    this._getHost();
+  }
+
+  _getHost = async() => {
+    const res = await axios.get('/api/host');
+    this.setState({ host : res.data.host })
+  }
+
+  render() {
+    return(
+      <div className='App'>
+        <h3> Welcome to <u> {this.state.host} </u> Blog! </h3>
+      </div>
+    )
+  }
+}
 
 export default App;
+
