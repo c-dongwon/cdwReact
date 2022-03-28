ae5a1d0156452ee5373fd1ee9e1b4539243775c4
c-dongwon <clzlqndqnd1@gmail.com>
2022-03-28 22:55:07 +0900

    220328

--- daily-react/src/hook/UseEffect.js
+++ daily-react/src/hook/UseEffect.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const Timer = () =>{
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            console.log("time is chkine")
+        },1000)
+        
+        return () => {
+            clearInterval(timer)
+        }
+    }, []);
+
+    return(
+        <div>
+            <span>타이머!!!</span>
+        </div>
+    )
+}
 
 const UseEffect = () => {
+    const [showTimer, setShowTimer] = useState(false)
     return (
         <div>
-            
+            {showTimer && <Timer/>}
+            <button onClick={() => setShowTimer(!showTimer)}>toggle</button>
         </div>
     );
 };
